f5b0a3dfe03d2309fab050eb8760cf11480cb55b
luckyahh <116437898+luckyahh@users.noreply.github.com>
2023-10-20 17:56:30 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Latihan 2 </title>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <body>
+    
+        
+        
+        <form>
+        <div class="mb-3">
+          <label for="input" class="fxorm-label">Input 1 (angka) </label>
+          <input type="number" name = "input" class="form-control">
+        </div>
+
+        <div class="mb-3">
+          <p> Pilih Input 2</p>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Choose</option>
+            <option value="satu">+</option>
+            <option value="dua">-</option>
+            <option value="tiga">x</option>
+            <option value="empat">/</option>
+        </select>
+        </div>
+        <button type="SUBMIT" class="btn btn-primary">Submit</button>
+      </form>
+
+        
+
+        
+        
+
+            
+
+    </body>
+
+</head>
+</html>
